445628617a0493db85be00db01dccbcb4d74373e
Ahmad Hendi <ahmadhndi10@gmail.com>
2021-02-16 16:09:13 +0100

    add the contact me without backend

---
+++ public/js/swipEffect.js
@@ -0,0 +1,9 @@
+
+
+
+function toggleForm(event) {
+    
+    let containerContactMe = document.querySelector('.container-contact-me');
+    containerContactMe.classList.toggle('active')
+    event.preventDefault()
+}
