5f87d2a7ac8bee28a9ccd10fecd8c2cee2ddb744
Ahmad Hendi <ahmadhndi10@gmail.com>
2021-07-02 14:54:16 +0200

    working on responsive

--- public/js/changeVidoe.js
+++ public/js/changeVidoe.js
@@ -2,7 +2,7 @@
 window.addEventListener('resize', () => {
     let screenWidth = $(window).width();
     let video = document.querySelector('#resumeVideo')
-    if (screenWidth < 600) {
+    if (screenWidth < 700) {
         video.src = '/public/videos/resumeVideo1080.mp4'
     }else{
         video.src = '/public/videos/ResumeVideo.mov'
