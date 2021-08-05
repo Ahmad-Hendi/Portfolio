5b9dbe1afbed7659498b0c47e02d2e56e2d6708a
Ahmad Hendi <ahmadhndi10@gmail.com>
2021-08-05 15:45:06 +0200

    me test

--- public/js/hoverScript.js
+++ public/js/hoverScript.js
@@ -3,28 +3,28 @@ let screenWidth = $(window).width();
 // console.log(screenWidth);
 
 
-if (screenWidth < 700) {
-    new hoverEffect ({
-        parent: document.querySelector('.img'),
-        intensity: 1,
-        imagesRatio: 1.2,
-        image1: '../public/images/aboutme2Small.png',
-        image2: '../public/images/aboutme2Small.png',
-        displacementImage: '../public/images/diss.png',
+// if (screenWidth < 700) {
+//     new hoverEffect ({
+//         parent: document.querySelector('.img'),
+//         intensity: 1,
+//         imagesRatio: 1.2,
+//         image1: '../public/images/aboutme2Small.png',
+//         image2: '../public/images/aboutme2Small.png',
+//         displacementImage: '../public/images/diss.png',
         
-    });
+//     });
    
-} else {
-    new hoverEffect ({
-        parent: document.querySelector('.img'),
-        intensity: 1,
-        imagesRatio: 1.2,
-        image1: '../public/images/aboutme2.png',
-        image2: '../public/images/aboutme1.png',
-        displacementImage: '../public/images/diss.png',
+// } else {
+//     new hoverEffect ({
+//         parent: document.querySelector('.img'),
+//         intensity: 1,
+//         imagesRatio: 1.2,
+//         image1: '../public/images/aboutme2.png',
+//         image2: '../public/images/aboutme1.png',
+//         displacementImage: '../public/images/diss.png',
         
-    });
-}
+//     });
+// }
 
 
 
