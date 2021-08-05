bccf86b8a647be2e72101d088fbaab957e157205
Ahmad Hendi <ahmadhndi10@gmail.com>
2021-08-05 16:59:34 +0200

    working on i phone

--- public/js/hoverScript.js
+++ public/js/hoverScript.js
@@ -1,32 +1,24 @@
-let screenWidth = $(window).width();
+// let screenWidth = $(window).width();
 
 // console.log(screenWidth);
 
 
 // if (screenWidth < 700) {
-//     new hoverEffect ({
-//         parent: document.querySelector('.img'),
-//         intensity: 1,
-//         imagesRatio: 1.2,
-//         image1: '../public/images/aboutme2Small.png',
-//         image2: '../public/images/aboutme2Small.png',
-//         displacementImage: '../public/images/diss.png',
-        
-//     });
+//     let myPersoPic = document.getElementsByClassName('.img')
+
+//     myPersoPic.html("<img src='images-min.jpg'/>")
    
-// } else {
-//     new hoverEffect ({
-//         parent: document.querySelector('.img'),
-//         intensity: 1,
-//         imagesRatio: 1.2,
-//         image1: '../public/images/aboutme2.png',
-//         image2: '../public/images/aboutme1.png',
-//         displacementImage: '../public/images/diss.png',
-        
-//     });
 // }
 
-
+// new hoverEffect ({
+//     parent: document.querySelector('.img'),
+//     intensity: 1,
+//     imagesRatio: 1.2,
+//     image1: '../public/images/aboutme2.png',
+//     image2: '../public/images/aboutme1.png',
+//     displacementImage: '../public/images/diss.png',
+    
+// });
 
 // services section start //
 /* 
