3fa2e578350ea6af47eb26cf716a5d6f62ef3d2f
Ahmad Hendi <ahmadhndi10@gmail.com>
2021-08-05 17:26:22 +0200

    add a simple loader

--- public/js/hoverScript.js
+++ public/js/hoverScript.js
@@ -1,24 +1,26 @@
-// let screenWidth = $(window).width();
+let screenWidth = $(window).width();
 
 // console.log(screenWidth);
 
 
-// if (screenWidth < 700) {
-//     let myPersoPic = document.getElementsByClassName('.img')
 
-//     myPersoPic.html("<img src='images-min.jpg'/>")
-   
-// }
-
-// new hoverEffect ({
-//     parent: document.querySelector('.img'),
-//     intensity: 1,
-//     imagesRatio: 1.2,
-//     image1: '../public/images/aboutme2.png',
-//     image2: '../public/images/aboutme1.png',
-//     displacementImage: '../public/images/diss.png',
+    // let myPersoPic = document.getElementsByTagName('#imgMe')
+    // console.log('yes');
+    // new hoverEffect ({
+    // parent: document.getElementsByTagName('#imgMe'),
+    // intensity: 1,
+    // imagesRatio: 1.2,
+    // image1: '../public/images/aboutme2.png',
+    // image2: '../public/images/aboutme1.png',
+    // displacementImage: '../public/images/diss.png',
     
-// });
+
+
+
+   
+
+
+
 
 // services section start //
 /* 
