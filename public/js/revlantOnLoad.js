a839e25271856967b5175d941dd78278ed6a2ed3
Ahmad <ahmadhndi10@gmail.com>
2021-08-17 02:13:20 +0200

    add block reval effect

--- public/js/revlantOnLoad.js
+++ public/js/revlantOnLoad.js
@@ -14,9 +14,9 @@ const nav = document.querySelector('.navBar');
 
 const tl = new TimelineMax();
 
-tl.fromTo(resumeVideo, 1, { opacity: 0, ease: Power1.easInOut}, { opacity: 1, ease: Power2.easInOut} )
-tl.fromTo(nav, 0.5, { opacity: 0, ease: Power1.easInOut}, { opacity: 1, ease: Power2.easInOut} )
-tl.fromTo(socialMedia, 1, { opacity: 0, ease: Power1.easInOut}, { opacity: 1, ease: Power2.easInOut} )
+tl.fromTo(resumeVideo, 3, { opacity: 0, ease: Power1.easInOut}, { opacity: 1, ease: Power2.easInOut}, 9 )
+tl.fromTo(nav, 3, { opacity: 0, ease: Power1.easInOut}, { opacity: 1, ease: Power2.easInOut}, 9)
+tl.fromTo(socialMedia, 1, { opacity: 0, ease: Power1.easInOut}, { opacity: 1, ease: Power2.easInOut}, 9 )
 // tl.fromTo(slider, 2, {height: "120%", ease: Power2.easInOut}, {height: "0%", ease: Power2.easInOut, })
 
 
